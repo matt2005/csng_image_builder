180ed7d855ce61571a3284dd314944ff42421564
matt <matthilton2005@gmail.com>
2025-10-04 07:39:57 +0100

    fix: Handle wiringpi package removal in Debian Trixie
    - Changed apt-get purge wiringpi to non-failing (|| true)
    - Resolves E: Unable to locate package wiringpi error
    - WiringPi was deprecated and removed from Debian repositories
    - Still installs from local .deb file as intended
    
    This allows the build to continue when wiringpi package is not available in the repository while still properly installing the local wiringpi-latest.deb package.

--- crankshaft/stage3/03-crankshaft-base/06-run-chroot.sh
+++ crankshaft/stage3/03-crankshaft-base/06-run-chroot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
-apt-get purge wiringpi -y
+# Try to purge wiringpi if it exists (may not be available in newer Debian versions)
+apt-get purge wiringpi -y || true
 hash -r
 dpkg -i /root/wiringpi-latest.deb
