1dca327161f9521bca3b12b47777569ba03c5ab0
matt <matthilton2005@gmail.com>
2025-11-01 11:43:24 +0000

    fix install

--- stages/stage60/01-opencardev-tweaks/01-run-chroot.sh
+++ stages/stage60/01-opencardev-tweaks/01-run-chroot.sh
@@ -3,12 +3,9 @@
 # Add OpenCarDev tweaks
 echo "Adding OpenCarDev tweaks..."
 # This runs in chroot context so we can use normal file paths
-# Copy zram-generator configuration
-install -Dm644 files/etc/systemd/zram-generator.conf /etc/systemd/zram-generator.conf
 systemctl daemon-reload && systemctl start systemd-zram-setup@zram0.service
 
 # Apply sysctl tweaks
-install -Dm644 files/etc/sysctl.conf /etc/sysctl.conf
 sysctl --system
 
 echo "OpenCarDev tweaks setup completed successfully"
